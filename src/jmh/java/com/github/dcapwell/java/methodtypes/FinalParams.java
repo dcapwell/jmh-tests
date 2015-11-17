84a8b60920f436c13310862133ab57b9d6583e1f
David Capwell <dcapwell@gmail.com>
2015-11-16 20:33:30 -0800

    added git ignore

---
+++ src/jmh/java/com/github/dcapwell/java/methodtypes/FinalParams.java
@@ -0,0 +1,11 @@
+package com.github.dcapwell.java.methodtypes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class FinalParams {
+    private static final Logger logger = LoggerFactory.getLogger(FinalParams.class);
+
+    private FinalParams() {
+    }
+}
