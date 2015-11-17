5109c439097146c882336c01777cd53ee72cbf26
David Capwell <dcapwell@gmail.com>
2015-11-16 21:27:15 -0800

    added jmh for final params

--- src/jmh/java/com/github/dcapwell/java/methodtypes/FinalParams.java
+++ src/jmh/java/com/github/dcapwell/java/methodtypes/FinalParams.java
@@ -1,11 +1,40 @@
 package com.github.dcapwell.java.methodtypes;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.CompilerControl;
 
-public final class FinalParams {
-    private static final Logger logger = LoggerFactory.getLogger(FinalParams.class);
+import java.util.concurrent.ThreadLocalRandom;
 
-    private FinalParams() {
+/**
+ * Shows that final params have no effect on runtime performance
+ */
+public class FinalParams {
+    private static final int left = ThreadLocalRandom.current().nextInt();
+    private static final int right = ThreadLocalRandom.current().nextInt();
+
+    @Benchmark
+    public int doNothing() {
+        // if below tests equal this one, then test bug
+        return 4;
+    }
+
+    @Benchmark
+    public int nonFinal() {
+        return add(left, right);
+    }
+
+    @Benchmark
+    public int withFinal() {
+        return addFinal(left, right);
+    }
+
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    private static int add(int left, int right) {
+        return left + right;
+    }
+
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    private static int addFinal(final int left, final int right) {
+        return left + right;
     }
 }
